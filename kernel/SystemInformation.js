9f5f996c0becd6eaf20434de742b353527ac262c
AboutUip <18234679734@163.com>
2026-01-09 03:14:41 +0800

    版本递增

--- kernel/SystemInformation.js
+++ kernel/SystemInformation.js
@@ -5,7 +5,7 @@ KernelLogger.info("SystemInformation", "模块初始化");
 
 class SystemInformation {
     // 系统版本
-    static SYSTEM_VERSION = '0.5.7';
+    static SYSTEM_VERSION = '0.5.8';
     
     // 内核版本
     static KERNEL_VERSION = '0.6.0';
